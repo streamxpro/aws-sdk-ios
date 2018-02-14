beeefdfc2b74909ea1552e1088ab849238839ff1
ikar <bolegvv@gmail.com>
2018-02-14 11:58:53 -0500

    [Feature] Added build of AWSSESCore

--- Package.swift
+++ Package.swift
@@ -2,11 +2,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "AWSCore",
+    name: "AWSSDK",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         //.library(name: "AWSAuth", targets: ["AWSAuth"]),
-        .library(name: "AWSCore", targets: ["AWSCore"]),
+        .library(name: "AWSSESCore", targets: ["AWSSESCore"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -14,7 +14,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "AWSCore",
+            name: "AWSSESCore",
             dependencies: [],
             path: "./AWSCore/"
             ),
